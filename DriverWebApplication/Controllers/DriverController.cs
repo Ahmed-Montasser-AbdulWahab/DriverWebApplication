397625098341ba7785b6e099345440bd6a21e961
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-19 17:30:46 +0200

    Add project files.

---
+++ DriverWebApplication/Controllers/DriverController.cs
@@ -0,0 +1,60 @@
+﻿using DriverWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DriverWebApplication.Controllers
+{
+    [Controller]
+    public class DriverController : Controller
+    {
+        private static IList<Driver> drivers = new List<Driver>();
+
+        [HttpGet]
+        [Route("/")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return Content("<h1>Welcome to Driver Portal</h1>","text/html");
+        }
+
+        [HttpGet]
+        [Route("all-drivers/{driverNationalId:regex(^[[2-3]][[0-9]]{{13}}$)?}")]
+        public IActionResult GetAllDrivers(string? driverNationalId)
+        {
+            if (driverNationalId == null)
+            {
+                return drivers.Count == 0 ? Content("No drivers are added."):Json(drivers);
+            }
+            else
+            {
+                foreach (Driver driver in drivers)
+                {
+                    if (driver.NationalId == driverNationalId)
+                    {
+                        return Json(driver);
+                    }
+                }
+            }
+
+            return NotFound($"Driver with national Id {driverNationalId} is not found.");
+        }
+
+        [HttpPost]
+        [Route("add-driver")]
+        public IActionResult AddDriver([FromForm] Driver driver)
+        {
+            if(!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage);
+
+                string message = string.Join("\n", errors);
+
+                return BadRequest(message);
+            }
+
+            drivers.Add(driver);
+
+            return Ok($"Driver {driver.NationalId} is successfully added.");
+        }
+
+    }
+}
---
+++ DriverWebApplication/Models/Driver.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriverWebApplication.Models
+{
+    public class Driver
+    {
+        [Required(ErrorMessage = "{0} must be provided.")]
+        [Display(Name = "National Id")]
+        [RegularExpression("^(2|3)[0-9]{13}$" , ErrorMessage = "{0} must be 14 digits starting with 2 or 3.")]
+        public string? NationalId { get; set; }
+
+        [Required(ErrorMessage = "Please provide your {0}")]
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage ="Enter your {0} such that it doesn't exceed {1} letters.")]
+        public string? FName { get; set; }
+
+        [Required(ErrorMessage = "Please provide your {0}")]
+        [Display(Name = "Last Name")]
+        [StringLength(20, ErrorMessage = "Enter your {0} such that it doesn't exceed {1} letters.")]
+        public string? LName { get; set; }
+
+
+        [Required(ErrorMessage = "Please provide your {0}.")]
+        [Display(Name = "Date of birth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter your Email Correctly")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone(ErrorMessage = "Enter your {0} correctly")]
+        public string? PhoneNumber { get; set; }
+
+    }
+}
